f5178586361a7dd7a64f4cadf426e3a91183818a
irfshaikh-01 <irfan161089@gmail.com>
2026-05-11 12:13:30 +0530

    tried random_state = 432 when when slpitting but removed it since its not needed.

--- train_model.py
+++ train_model.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 
 X, Y = fetch_openml(name = 'mnist_784', version = 1, return_X_y = True)
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state = 432)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
 
 model = RandomForestClassifier(n_jobs = -1)
 
